da5f326e13f2d0ab276ffa6715296657f02a122f
dekunukem <dekunukem@gmail.com>
2020-08-16 13:28:54 +0100

    board hello world

---
+++ firmware/Inc/helpers.h
@@ -0,0 +1,16 @@
+#ifndef __HELPERS_H
+#define __HELPERS_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif 
+
+#include "stm32f0xx_hal.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
---
+++ firmware/Src/helpers.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "helpers.h"
